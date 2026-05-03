e4e30b223a551226f3b3c80ed1501c5c4700875b
Matthew <tatoba2351@gmail.com>
2026-05-03 15:14:45 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-green-50 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-green-800">Ager</h1>
+    </div>
+  );
+}
